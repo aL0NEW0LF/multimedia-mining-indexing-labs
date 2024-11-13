5c3ba48d0600ab6bb025a05e56f75e9340cebc3c
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2024-11-13 16:33:55 +0100

    refactor: change select point to roi

--- lab-4/q-1.py
+++ lab-4/q-1.py
@@ -1,17 +1,18 @@
+# TODO: - Fix the bug: nan values generated in the responses, which translates to the features and similarity
+
 import cv2
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 
 
-def build_gabor_filters(num_orientations=4, num_scales=3, kernel_size=31):
+def build_gabor_filters(num_orientations=4, num_scales=3, kernel_size=11):
     filters = []
-    for theta in range(num_orientations):
-        theta = (theta * np.pi) / num_orientations
+    for theta in np.arange(0, np.pi, np.pi / num_orientations):
         for scale in range(num_scales):
-            sigma = 3.0
-            lamda = 4.0 * (2**scale)
-            gamma = 0.5
+            sigma = 2 * np.pi * scale / num_scales
+            lamda = np.pi / 4
+            gamma = 0.25
             psi = 0
             kernel = cv2.getGaborKernel(
                 (kernel_size, kernel_size), sigma, theta, lamda, gamma, psi
@@ -28,18 +29,19 @@ def apply_gabor_filters(image, filters):
     return responses
 
 
-def extract_features(responses, x, y, window_size=9):
+def extract_features(responses, x, y, window_size=11):
     features = []
     half_window = window_size // 2
     height, width = responses[0].shape
 
-    if (
-        x - half_window < 0
-        or x + half_window >= height
-        or y - half_window < 0
-        or y + half_window >= width
-    ):
-        return np.array([np.nan] * 4 * len(responses))
+    # if (
+    # x - half_window < 0
+    # or x + half_window >= height
+    # or y - half_window < 0
+    # or y + half_window >= width
+    # ):
+    # return np.array([np.nan] * 4 * len(responses))
+    # return np.array([np.nan] * len(responses))
 
     for response in responses:
         feature = response[
@@ -47,19 +49,31 @@ def extract_features(responses, x, y, window_size=9):
         ]
 
         mean = np.mean(feature)
-        std = np.std(feature)
-        skewness = stats.skew(feature.flatten())
-        kurtosis = stats.kurtosis(feature.flatten())
+        # std = np.std(feature)
+        # skewness = stats.skew(feature.flatten())
+        # kurtosis = stats.kurtosis(feature.flatten())
 
-        features.extend([mean, std, skewness, kurtosis])
+        # features.extend([mean, std, skewness, kurtosis])
+        features.append(mean)
 
     return np.array(features)
 
 
-def segment_features(image, selected_x, selected_y, window_size=5, threshold=0.5):
+def segment_features(image, selected_roi, window_size=11, threshold=0.01):
     filters = build_gabor_filters()
     responses = apply_gabor_filters(image, filters)
-    ref_features = extract_features(responses, selected_x, selected_y, window_size)
+    reference_responses = apply_gabor_filters(selected_roi, filters)
+    reference_features = np.zeros(
+        (len(selected_roi), len(selected_roi[0]), len(filters))
+    )
+    print(f"Reference responses: {reference_responses}")
+    for x in range(len(selected_roi)):
+        for y in range(len(selected_roi[0])):
+            reference_features[x, y] = extract_features(
+                reference_responses, x, y, window_size
+            )
+    print(f"Reference features: {reference_features}")
+    reference_feature = np.mean(reference_features, axis=(0, 1))
 
     height, width = image.shape
     mask = np.zeros((height, width))
@@ -68,21 +82,15 @@ def segment_features(image, selected_x, selected_y, window_size=5, threshold=0.5
     for x in range(height):
         for y in range(width):
             current_features = extract_features(responses, x, y, window_size)
-            similarity = np.corrcoef(ref_features, current_features)[0, 1]
-
+            similarity = np.dot(reference_feature, current_features) / (
+                np.linalg.norm(reference_feature) * np.linalg.norm(current_features)
+            )
             if similarity > threshold:
                 mask[x, y] = 1
 
             i += 1
-            print(f"Processed {i} pixels of {height * width}", end="\r")
+            # print(f"Processed {i} pixels of {height * width}", end="\r")
     return mask
-
-
-def select_point(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        param["x"] = x
-        param["y"] = y
-        param["selected"] = True
 
 
 def main():
@@ -90,16 +98,14 @@ def main():
     img = cv2.imread("assets/a.jpg", 0)
     cv2.namedWindow("Select Point")
     cv2.imshow("Select Point", img)
-    params = {"x": -1, "y": -1, "selected": False}
-    cv2.setMouseCallback("Select Point", select_point, params)
-    while not params["selected"]:
-        if cv2.waitKey(20) & 0xFF == 27:
-            break
-    print(f"Selected point: ({params['x']}, {params['y']})")
+    roi = cv2.selectROI("Select Point", img)
+    selected_roi = img[
+        int(roi[1]) : int(roi[1] + roi[3]), int(roi[0]) : int(roi[0] + roi[2])
+    ]
     cv2.destroyAllWindows()
 
     # segment the image using the selected point
-    mask = segment_features(img, params["x"], params["y"], window_size=5, threshold=0.8)
+    mask = segment_features(img, selected_roi, threshold=0.2)
     print(f"Segmented image: {mask}")
     cv2.imshow("Segmented Image", mask)
     cv2.waitKey(0)
